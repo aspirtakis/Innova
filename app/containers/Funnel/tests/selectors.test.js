62a7b4e98916069f02eb4c8d5dbd684d4a26b049
Agamemnon <aspirtakis@gmail.com>
2019-12-08 22:18:30 +0100

    rebase to develop

---
+++ app/containers/Funnel/tests/selectors.test.js
@@ -0,0 +1,7 @@
+// import { selectFunnelDomain } from '../selectors';
+
+describe('selectFunnelDomain', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
