f60bef18ae793a94a82f5fa2ec43c78422826f59
Agamemnon <aspirtakis@gmail.com>
2020-05-12 15:40:10 +0200

    config

--- app/utils/config.js
+++ app/utils/config.js
@@ -25,19 +25,7 @@ const backendDev = {
   defUser: '',
   defPass: '',
 };
-const backendAga = {
-  beUrl: 'https://aws.openinnovationhub.nl/api/v2/',
-  apptoken: 'cfe595a88b10a4aa5ef460660f6240bd3a72f89e411d31169579444145119f89',
-  sessionUrl: 'user/session',
-  system: 'system/user?related=user_to_app_to_role_by_user_id&order=name%20ASC',
-  tasks: 'funnel_dev/_table/funnel.tasks',
-  remarks: 'funnel_dev/_table/funnel.remarks',
-  assumptions: 'funnel_dev/_table/funnel.assumptions',
-  checklists: 'funnel_dev/_table/funnel.checklists',
-  stageGates: 'funnel_dev/_table/funnel.stagegates',
-  defUser: 'agamemnon.aspirtakis@kpn.com',
-  defPass: 'a224935a',
-};
+
 const backendAgaSTG = {
   beUrl: 'https://aws.openinnovationhub.nl/api/v2/',
   apptoken: 'cfe595a88b10a4aa5ef460660f6240bd3a72f89e411d31169579444145119f89',
@@ -68,5 +56,5 @@ const backendSTG = {
 
 
 
-const backend = backendAgaSTG;
-export default { backend };
+const backend = backendDev;
+export { backend };
