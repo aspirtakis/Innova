eb4fe4e830ae263617eff0e56ffe1aa298a07e4c
Agamemnon <aspirtakis@gmail.com>
2020-05-13 11:18:28 +0200

    reverse config

--- app/utils/config.js
+++ app/utils/config.js
@@ -25,7 +25,19 @@ const backendDev = {
   defUser: '',
   defPass: '',
 };
-
+const backendAga = {
+  beUrl: 'https://aws.openinnovationhub.nl/api/v2/',
+  apptoken: 'cfe595a88b10a4aa5ef460660f6240bd3a72f89e411d31169579444145119f89',
+  sessionUrl: 'user/session',
+  system: 'system/user?related=user_to_app_to_role_by_user_id&order=name%20ASC',
+  tasks: 'funnel_dev/_table/funnel.tasks',
+  remarks: 'funnel_dev/_table/funnel.remarks',
+  assumptions: 'funnel_dev/_table/funnel.assumptions',
+  checklists: 'funnel_dev/_table/funnel.checklists',
+  stageGates: 'funnel_dev/_table/funnel.stagegates',
+  defUser: 'agamemnon.aspirtakis@kpn.com',
+  defPass: 'a224935a',
+};
 const backendAgaSTG = {
   beUrl: 'https://aws.openinnovationhub.nl/api/v2/',
   apptoken: 'cfe595a88b10a4aa5ef460660f6240bd3a72f89e411d31169579444145119f89',
@@ -50,11 +62,10 @@ const backendSTG = {
   checklists: 'FunnSTG/_table/funnel.checklists',
   stageGates: 'FunnSTG/_table/funnel.stagegates',
   defUser: '',
-  defPass: '1',
+  defPass: '',
 };
 
 
 
-
-const backend = backendAgaSTG;
+const backend = backendSTG;
 export { backend };
