36ec282d9386d6aeee639171b6f5aff4b9fa7a7d
Agamemnon <aspirtakis@gmail.com>
2020-08-24 15:13:12 +0200

    move container

---
+++ app/containers/Initiatives/actions.js
@@ -0,0 +1,13 @@
+/*
+ *
+ * Initiatives actions
+ *
+ */
+
+import { DEFAULT_ACTION } from "./constants";
+
+export function defaultAction() {
+  return {
+    type: DEFAULT_ACTION
+  };
+}
---
+++ app/containers/Initiatives/constants.js
@@ -0,0 +1,7 @@
+/*
+ *
+ * Initiatives constants
+ *
+ */
+
+export const DEFAULT_ACTION = "app/Initiatives/DEFAULT_ACTION";
