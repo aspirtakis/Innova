88ccefb65bdd7d37bc4099922fd5af1ba5ccf42f
Agamemnon <aspirtakis@gmail.com>
2020-09-04 11:20:34 +0200

    fixe general

--- app/containers/Initiatives/index.js
+++ app/containers/Initiatives/index.js
@@ -25,6 +25,7 @@ import ReactQuill from 'react-quill'; // ES6
 import EditableTable from '../../components/editableTable';
 import StageGates from '../../components/stagegates';
 import { size } from 'lodash';
+import './initiativesStyles.css';
 
 
 import { Tab, TabItem, TabLink, TabMenu } from '@kpn-style/react';
@@ -45,50 +46,66 @@ class Initiatives extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      hasSelectedcard: false,
+    };
+
   }
   componentWillMount(){
-  //  this.props.sessionCheck();
-    console.log(this.props);
-const data = this.props.location.state.data;
-const gates = data.stageGates;
-this.setState({
-  users:this.props.location.users,
-  spinning: false,
-  cardPO:data.cardpo,
-  funnel: data.funnel,
-  projectname: data.projectname,
-  description: data.description,
-  horizon: data.horizon,
-  theme: data.theme,
-  status: data.status,
-  FunnelPhase: data.FunnelPhase,
-  coach: data.coach,
-  sponsor: data.sponsor,
-  task_id: data.task_id,
-  createDate: data.createDate,
-  spnsr: data.spnsr,
-  remarks:data.remarks ? data.remarks.slice().sort((a, b) => new Date(b.created) - new Date(a.created)) : data.remarks,
-  value:data.value,
-  value:data.value,
-  prjcost:data.prjcost,
-  assumptions:data.assumptions,
-  nexStageGate:data.nexStageGate,
-  stageGates: gates ? gates.slice().sort((a, b) => new Date(b.created) - new Date(a.created)) : gates ,
-  
-});
+const data = this.props.location.state ? this.props.location.state.data :null ;
+if(!data){
+  this.setState({
+      hasSelectedcard: false,
+  });
+}
+if(data){
+  const gates = data.stageGates;
+  this.setState({
+    hasSelectedcard: true,
+    users:this.props.location.state.users,
+    spinning: false,
+    cardPO:data.cardpo,
+    funnel: data.funnel,
+    projectname: data.projectname,
+    description: data.description,
+    horizon: data.horizon,
+    theme: data.theme,
+    status: data.status,
+    FunnelPhase: data.FunnelPhase,
+    coach: data.coach,
+    sponsor: data.sponsor,
+    task_id: data.task_id,
+    createDate: data.createDate,
+    spnsr: data.spnsr,
+    remarks:data.remarks ? data.remarks.slice().sort((a, b) => new Date(b.created) - new Date(a.created)) : data.remarks,
+    value:data.value,
+    value:data.value,
+    prjcost:data.prjcost,
+    assumptions:data.assumptions,
+    nexStageGate:data.nexStageGate,
+    stageGates: gates ? gates.slice().sort((a, b) => new Date(b.created) - new Date(a.created)) : gates ,   
+  });
+
+}
+
 
   }
 
   componentWillReceiveProps(next) {
 
-    const  data  = next.location.state.data;
+    const  data  = next.location.state ? next.location.state.data : null;
     console.log(next);
+    if(!data){
+      this.setState({
+          hasSelectedcard: false,
+      });
+    }
 
 //const test1 = next.users && next.users.map(allusers => allusers.user_to_app_to_role_by_user_id);
-
+if(data){
 const gates = data.stageGates;
     this.setState({
-      users:next.users,
+      users:next.location.state.users,
       spinning: false,
       cardPO:data.cardpo,
       funnel: data.funnel,
@@ -112,6 +129,7 @@ const gates = data.stageGates;
       stageGates: gates ? gates.slice().sort((a, b) => new Date(b.created) - new Date(a.created)) : gates ,
       
     });
+  }
   }
 
   saveChecklist = (r,row) => {
@@ -661,7 +679,7 @@ const gates = data.stageGates;
       })
       .then(response => response.json())
       .then(taskData => {
-      //  this.props.onCancel();
+
         this.setState({ spinning: false });
       })
       .catch(taskData => console.log(taskData));
@@ -707,7 +725,7 @@ const gates = data.stageGates;
       })
       .then(response => response.json())
       .then(taskData => {
-        this.props.onCancel();
+      //  this.props.onCancel();
         this.setState({ spinning: false });
         this.props.onOK();
       })
@@ -736,7 +754,7 @@ const gates = data.stageGates;
       })
       .then(response => response.json())
       .then(taskData => {
-        this.props.onCancel();
+      //  this.props.onCancel();
         this.setState({ spinning: false });
         this.props.onOK();
       })
@@ -774,41 +792,336 @@ const gates = data.stageGates;
       </Col>
     </Row>
   );
+  fixTitles = () =>
+  {
+    const titles =  this.state.cardPO === this.props.location.state.user.first_name + " " + this.props.location.state.user.last_name ? data.projectname + "overview - You are PO of this project" : data.projectname + " overview";
+
+  };
+
+  remarksType = (type) =>
+  {
+  const TeamRemarks = this.state.remarks && this.state.remarks.filter(city => city.type === "User");
+  const CoachRemarks = this.state.remarks && this.state.remarks.filter(city => (city.type == null || city.type === "Coach"));
+  if(type === "coach"){
+    return CoachRemarks;
+  }
+  if(type === "team"){
+    return TeamRemarks;
+  }
+  
+  };
 
   
 
 
 
   render() {
-    const { visible, onOK, onCancel, user} = this.props.location.state;
 
     const  data  = this.state;
-    const titles =  this.state.cardPO === this.props.location.state.user.first_name + " " + this.props.location.state.user.last_name ? data.projectname + "overview - You are PO of this project" : data.projectname + " overview";
-    const rema =this.state.remarks;
-
- const TeamRemarks = rema && rema.filter(city => city.type === "User");
- const CoachRemarks = rema && rema.filter(city => (city.type == null || city.type === "Coach"));
-
-//  const remUser = remarks.filter(mak => mak.type === "User");
 
     return (
       <div className="row">
-      <div className="col col--4">
-        <Tab>
-          <TabMenu>
-            <TabItem>
-              <TabLink active>
-                Home
-              </TabLink>
-            </TabItem>
-            <TabItem>
-              <TabLink>
-                Services
-              </TabLink>
-            </TabItem>
-          </TabMenu>
-        </Tab>
-      </div>
+      {this.state.hasSelectedcard ? 
+        <div
+      >
+       <div className=" mainContainer">
+       <div className="titleContainer">
+       <div className="titleInit">{this.state.projectname}</div>
+       </div>
+    <Tabs tabBarStyle={{borderBlockColor:"#009900", color:'green', fontFamily:"kpn-metric-bold",  }} className="mainTab">
+      <TabPane  tab={<span className="titlesTab"> General</span>} key="1">
+      <div>
+                <Row>
+                  <Col span={8}>
+                  <p className="titleInit">Team Members </p>
+                  <p className="titleGeneral" >Product Owner </p>
+                  <p className="dataGeneral" > {data.cardPO} </p>
+
+                  <p className="titleGeneral" >Coach </p>
+                  <p className="dataGeneral" > {data.coach} </p>
+       
+
+                  <p className="titleGeneral" >Sponsor </p>
+                  <p className="dataGeneral" > {data.spnsr} </p>
+
+                  <p className="titleGeneral" >Growth Hacker  </p>
+                  <p className="dataGeneral" > XXXXXX </p>
+
+                  <p className="titleGeneral" >Data Expert  </p>
+                  <p className="dataGeneral" > XXXXXX </p>
+                  <p>Created Date : {data.createDate} </p>
+                  <p>Added :{moment(data.createDate).fromNow()}</p>
+                  <p>Updated :{moment(data.updateDate).fromNow()}</p>
+                  <p>Updated :{moment(data.birthonproblem).fromNow()}</p>
+
+  
+                  </Col>
+                  <Col span={6}>
+                  <p className="titleInit">Project </p>
+
+                  <p className="titleGeneral" >Department</p>
+                  <p className="dataGeneral" > {data.funnel} </p>
+
+                  <p className="titleGeneral" >Theme</p>
+                  <p className="dataGeneral" > {data.theme} </p>
+
+                  <p className="titleGeneral" >Horizon</p>
+                  <p className="dataGeneral" > {data.horizon} </p>
+              
+                  <p className="titleGeneral" >Project Cost</p>
+                  <p className="dataGeneral" > {data.prjcost} </p>
+
+                  <p className="titleGeneral" >Value</p>
+                  <p className="dataGeneral" > {data.value} </p>
+
+                  <p className="titleGeneral" >Description</p>
+                  <p className="dataDescription" > {data.description} </p>
+
+                  <p className="titleGeneral" >Team Members</p>
+                  <p className="dataGeneral" > {data.sponsor} </p>
+
+                  <p className="titleGeneral" >Next StageGate</p>
+                  <p className="dataGeneral" > {data.nextStageGate} </p>
+
+
+                  </Col>
+                </Row>
+              </div>
+      </TabPane>
+     {(this.state.cardPO === this.props.location.state.user.first_name+" "+this.props.location.state.user.last_name || this.props.location.state.user.role === 'Coach') &&  <TabPane tab={<span className="titlesTab"> Update</span>} key="2">
+      <Form>
+<Form.Row>
+<Form.Group style={{flexWrap:"nowrap", marginLeft:10}} as={Col} controlId="ControlFunnel">
+    <Form.Label style={{ marginTop: 5 }}>Department</Form.Label>
+    <Form.Control
+      value={this.state.funnel}
+      onChange={e =>
+        this.setState({ funnel: e.target.value })
+      }
+      as="select"
+    >
+                      <option value="OIH">OIH</option>
+                      <option value="CM">CM</option>
+                      <option value="BM">BM</option>
+                      <option value="WS">WS</option>
+                      <option value="OPS">OPS</option>
+    </Form.Control>
+    <Form.Label style={{ marginTop: 5 }}>Theme</Form.Label>
+
+
+
+  <Form.Control
+  value={this.state.theme}
+  onChange={e => this.setState({ theme: e.target.value })}
+  as="select"
+>
+
+  <option value="NextGenInfra">Next-Gen Infra</option>
+  <option value="DataTech">Data Tech</option>
+  <option value="Techco">TechCo</option>
+  <option value="Other">Other</option>
+</Form.Control>
+
+    <Form.Label style={{ marginTop: 5 }}>Description</Form.Label>
+    <Form.Control
+      value={this.state.description}
+      onChange={e =>
+        this.setState({ description: e.target.value })
+      }
+      as="textarea"
+      rows="5"
+    />
+    </Form.Group>
+
+<Form.Group style={{flexWrap:"nowrap", marginLeft:10}} as={Col} controlId="ControlFunnel2"
+    style={{ marginLeft: 10 }}
+    as={Col}
+    controlId="Title"
+  >
+
+    <Form.Label style={{ marginTop: 5 }}>Project</Form.Label>
+    <Form.Control
+    value={this.state.projectname}
+    onChange={e => this.setState({ projectname: e.target.value })}
+    type="text"
+    placeholder="project name"
+  />
+
+
+    <Form.Label style={{ marginTop: 5 }}>Status</Form.Label>
+    <Form.Control
+      value={this.state.status}
+      onChange={e => this.setState({ status: e.target.value })}
+      as="select"
+    >
+      <option value="green">PROGRESSING</option>
+      <option value="yellow">IMPEDIMENT</option>
+      <option value="red">STOPPED </option>
+      <option value="orange">PARKED</option>
+    </Form.Control>
+    
+    <Form.Label style={{ marginTop: 5 }}>CardPO</Form.Label>
+    <Form.Control
+      value={this.state.cardPO}
+      onChange={e => this.setState({ cardPO: e.target.value })}
+      as="select"
+    >
+{this.state.users && this.state.users.map(username =>  <option  key={username.id} value={username.first_name +" " + username.last_name}>{username.first_name +" "+ username.last_name}</option>)}
+    </Form.Control>
+
+    <Form.Label style={{ marginTop: 5 }}>Coach</Form.Label>
+    <Form.Control
+      value={this.state.coach}
+      onChange={e => this.setState({ coach: e.target.value })}
+      as="select"
+    >
+    {this.state.users && this.state.users.map(username =>  <option  key={username.id} value={username.first_name +" " + username.last_name}>{username.first_name +" "+ username.last_name}</option>)}
+    </Form.Control>
+
+  </Form.Group>
+<Form.Group style={{flexWrap:"nowrap", marginLeft:10}}  as={Col} controlId="ControlFunnel3">
+  <Form.Label style={{ marginTop: 5 }}>Value</Form.Label>
+  <Form.Control
+    value={this.state.value}
+    onChange={e => this.setState({ value: e.target.value })}
+    type="text"
+    placeholder="Value"
+  />
+
+
+  <Form.Label style={{ marginTop: 5 }}>Project Cost</Form.Label>
+  <Form.Control
+    value={this.state.prjcost}
+    onChange={e => this.setState({ prjcost: e.target.value })}
+    type="number"
+    placeholder="Project Cost"
+  />
+
+  <Form.Label style={{ marginTop: 5 }}>Team Members</Form.Label>
+  <Form.Control
+    value={this.state.sponsor}
+    onChange={e => this.setState({ sponsor: e.target.value })}
+    as="select"
+  >
+    <option>1</option>
+    <option>2</option>
+    <option>3 </option>
+    <option>4</option>
+  </Form.Control>
+  <Form.Label style={{ marginTop: 5 }}>Sponsor</Form.Label>
+    <Form.Control
+      value={this.state.spnsr}
+      onChange={e => this.setState({ spnsr: e.target.value })}
+      type="text"
+      placeholder="Sponsor"
+    />
+
+  <Form.Label style={{ marginTop: 5 }}>Horizon</Form.Label>
+    <Form.Control
+      value={this.state.horizon}
+      onChange={e => this.setState({ horizon: e.target.value })}
+      as="select"
+    >
+      <option>H1</option>
+      <option>H2</option>
+      <option>H3</option>
+    </Form.Control>
+
+</Form.Group>
+</Form.Row>
+<Button onClick={this.onUpdate} variant="primary" type="submit">
+  Submit
+</Button>
+
+<Button style={{marginLeft:100}} onClick={this.onDelete} variant="danger" type="submit">
+  Delete
+</Button>
+</Form>
+      </TabPane>
+}
+
+
+{ this.props.location.state.user.role !== 'Tv' && 
+<TabPane tab={<span className="titlesTab"> Assumptions </span>} key="3">
+
+{ (this.state.cardPO === this.props.location.state.user.first_name + " " + this.props.location.state.user.last_name || this.props.location.state.user.role === 'Coach' || this.props.location.state.user.role === 'CardPO' || this.props.location.state.user.role === 'BO' ||  this.props.location.state.user.role === 'User' ) && 
+      <Button onClick={this.addNewAssumption} type="primary" style={{ marginBottom: 16 }}>
+      Create New
+    </Button>}
+
+      <EditableTable 
+      saveAssumption={this.saveAssumption} 
+      saveChecklist={this.saveChecklist} 
+      deleteChecklist={this.deleteChecklist}
+      deleteAssumption={this.deleteAssumption}
+      assumptions={this.state.assumptions}
+      addChecklist={this.addNewCheckList}
+      role={this.props.location.state.user.role}
+      />
+      </TabPane>}
+
+      { (this.props.location.state.user.role === 'Coach' || this.props.location.state.user.role === 'Manager' ) &&
+            <TabPane tab={<span className="titlesTab"> Coach remarks</span>} key="4">
+
+      { this.props.location.state.user.role === 'Coach'  && <Button onClick={() => this.addNewRemark("Coach")} type="primary" style={{  marginBottom: 16 }}>Create New
+      </Button>}
+        <Remarks onOK={this.props.onOK} deleteRemark={this.deleteRemark} coach={data.coach} user={this.props.location.state.user} saveRemark={this.saveRemark} remarks={  this.remarksType("coach")  }/>
+      </TabPane>}
+
+      {<TabPane tab={<span className="titlesTab"> Team remarks</span>} key="5">
+
+{ <Button onClick={() => this.addNewRemark("User")} type="primary" style={{  marginBottom: 16 }}>Create New
+</Button>}
+  <Remarks onOK={this.props.onOK} deleteRemark={this.deleteRemark} coach={data.coach} user={this.props.location.state.user} saveRemark={this.saveRemark} remarks={  this.remarksType("team") } />
+</TabPane>}
+       
+      <TabPane tab={<span className="titlesTab"> Meetings</span>} key="6">
+      
+      
+     {(this.state.cardPO === this.props.location.state.user.first_name + " " + this.props.location.state.user.last_name || this.props.location.state.user.role === 'Coach' || this.props.location.state.user.role === 'Manager') && 
+     <div > 
+      <Button 
+      onClick={() => this.addNewMeeting("StageGate")} 
+      type="primary" 
+      style={{  marginRight: 16,marginLeft: 16  }}>
+      Create SG
+      </Button>
+      <Button style={{  marginRight: 16  }} onClick={() => this.addNewMeeting("FundingMoment")} type="primary" style={{  marginBottom: 16 }}>Funding Momment
+</Button>
+<span style={{  marginLeft: 30  }}>
+Next Meeting :
+<DatePicker 
+
+showTime={{
+  hideDisabledOptions: true,
+}}
+
+value={moment(this.state.nexStageGate, dateFormat)}
+format={dateFormat}
+onChange={(date, dateString) => {
+  this.setState({nexStageGate: moment(date).format(dateFormat)});
+  this.onSTGUpdate(moment(date).format(dateFormat));
+} } />
+</span>
+      
+      </div>}
+    
+  <StageGates 
+  onOK={this.props.onOK} 
+  deleteMeeting={this.deleteMeeting}
+  user={this.props.location.state.user} 
+  saveMeeting={this.saveMeeting}
+   stageGates={this.state.stageGates} 
+   nextGate={this.state.nexStageGate} />
+</TabPane>
+
+<TabPane style={{fontSize:10 ,color:'white'}} tab={<span className="titlesTab"> Canvas</span>} key="7">
+
+</TabPane>
+    </Tabs>
+  </div>
+      </div>:<div>You have to Select a Card from funnel</div>
+      }
     </div>
     );
   }
