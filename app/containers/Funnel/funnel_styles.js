f14c5f2dca35ad563bcf9fc1564023e63a3f1744
Agamemnon <aspirtakis@gmail.com>
2020-10-24 19:20:17 +0200

    fixed spacing for funnel column title
    card horizon now right color (#333333)
    
    fix padding on cards
    
    fix SG subtitle font size
    
    fix SG title font size
    
    changed header logo to svg
    
    fix status dots (8px width/height)
    
    side menu icon color now black
    
    change login screen logo to svg, sizing not final
    
    changed sidemenu item text to #333

--- app/containers/Funnel/funnel_styles.js
+++ app/containers/Funnel/funnel_styles.js
@@ -94,7 +94,7 @@ export const styles = {
     justifyContent: 'center',
   },
   titles: {
-    fontSize: 28,
+    fontSize: 32,
     backgroundColor:'#F3F3F3',
     fontWeight: 'bold',
     color: '#009900',
@@ -126,7 +126,7 @@ export const styles = {
   },
 
   ColTitles: {
-    fontSize: 23,
+    fontSize: 24,
     marginTop: 8,
     marginLeft:6,
     fontFamily: 'kpn-metric-normal',
