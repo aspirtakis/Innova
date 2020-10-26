e3198be66788af7f4b67b0c84f8f7e2dd520510e
SB <47176495+SBDutchie@users.noreply.github.com>
2020-10-26 13:45:52 +0100

    remove unused fields

--- app/components/Agenda/addnewMeeting.js
+++ app/components/Agenda/addnewMeeting.js
@@ -7,7 +7,7 @@ import { DatePicker } from 'antd';
 import './agenda.css';
 import MeetingSelector from './subcomponents/meetingSelector';
 import moment from 'moment';
-const dateFormat = 'DD/MM/YYYY HH:mm:ss';
+const dateFormat = 'DD/MM/YYYY HH:mm';
 
 class AddNewMeeting extends React.Component {
   constructor(props) {
@@ -46,34 +46,21 @@ class AddNewMeeting extends React.Component {
                   title="Meeting type"
                   selectedValue={(e) => this.settingValueStates("meetingType", e)}
                 />
-
-                <InputSmall
-                  title="Meeting name"
-                  inputType="text"
-                  placeholder="Update meeting"
-                  smallInputValue={(e) => this.settingValueStates("meetingName", e)}
-                /> 
                 
-                {/* fix avatar dots */}
-                <div>
-                  <dt className="smallInputTitle">Invite</dt>
-                  <dd className="smallInputField">
-                    There should be avatars and an add button here!
-                    </dd>
-                </div>
-
-
+                <dt className="dt smallInputTitle">Meeting date</dt>
                 <DatePicker 
                 title="Meeting date"
                 showTime={{
                   hideDisabledOptions: true,
                 }}
                 
+                className="datePicker"
               //  value={moment(this.state.meetingDate, dateFormat)}
                 format={dateFormat}
                 onChange={(date, dateString) => {
                   this.settingValueStates("meetingDate", moment(date,dateFormat))
                 } } />
+
 
                 <InputSmall
                   title="Funding request"
@@ -81,15 +68,11 @@ class AddNewMeeting extends React.Component {
                   placeholder="€"
                   smallInputValue={(e) => this.settingValueStates("fundingRequest", e)}
                 />
+
                 <InputLarge
                   title="Goal"
                   placeholder="Explain why this meeting is required"
                   largeInputValue={(e) => this.settingValueStates("meetingGoal", e)}
-                />
-                <InputLarge
-                  title="Feedback"
-                  placeholder="Meeting feedback (to be added after the meeting)"
-                  largeInputValue={(e) => this.settingValueStates("meetingFeedback", e)}
                 />
                 
                 {/* 
@@ -100,7 +83,7 @@ class AddNewMeeting extends React.Component {
                 */}
 
               </dl>
-              <div className="addMeetingButtons">
+              <div className="meetingButtons">
                 <button onClick={this.saveData} className="button button--4">Send meeting</button>
                 <button onClick={addMeetingCancel} className="button button--link">Cancel</button>
               </div>
