13b7598de952527d649fb4f30a438e424bb28734
SB <47176495+SBDutchie@users.noreply.github.com>
2020-10-26 13:46:32 +0100

    fix rendering/remove unused fields

--- app/components/Agenda/updateMeeting.js
+++ app/components/Agenda/updateMeeting.js
@@ -4,9 +4,9 @@ import moment from 'moment';
 import InputSmall from "./subcomponents/inputSmall";
 import InputLarge from "./subcomponents/inputLarge";
 import MeetingSelector from "./subcomponents/meetingSelector";
-const dateFormat = 'DD/MM/YYYY HH:mm:ss';
+const dateFormat = 'DD/MM/YYYY HH:mm';
 import './agenda.css';
-import { Select ,DatePicker } from 'antd';
+import { Select, DatePicker } from 'antd';
 const { Option } = Select;
 import { Editor } from '@tinymce/tinymce-react';
 
@@ -15,7 +15,7 @@ class UpdateMeeting extends React.Component {
   constructor(props) {
     super(props);
     console.log(props);
-    const {meetingData} = props;
+    const { meetingData } = props;
 
     this.state = {
       id: meetingData.id,
@@ -41,50 +41,39 @@ class UpdateMeeting extends React.Component {
 
   render() {
     const { editMeetingCancel, meetingData } = this.props;
-  //  console.log(this.state);
+    //  console.log(this.state);
     return (
       <div className="content">
         <div className="content__body">
           <div className="row">
             <div className="col col--7">
               <dl className="dl">
+                <dt className="dt smallInputTitle">Meeting type</dt>
+                <Select
+                  className="meetingSelector"
+                  style={{ width: 200 }}
+                  placeholder="Select a type"
+                  value={this.state.meetingType}
+                  onChange={(value) => this.settingValueStates("meetingType", value)}
+                  filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                >
+                  <Option value="StageGate">Stagegate</Option>
+                  <Option value="FundingMoment">Funding moment</Option>
+                </Select>
 
-              <Select
-              style={{ width: 200 }}
-              placeholder="Select a person"
-              value={this.state.meetingType}
-              onChange={(value) => this.settingValueStates("meetingType", value)}
-              filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-            >
-              <Option value="StageGate">Stage Gate</Option>
-              <Option value="FundingMoment">Funding momment</Option>
-            </Select>
 
+                <dt className="dt smallInputTitle">Meeting date</dt>
+                <DatePicker
+                  showTime={{
+                    hideDisabledOptions: true,
+                  }}
 
-                {/* {/* fix avatar dots */}
-                {/* <div>
-                  <dt className="smallInputTitle">Invite</dt>
-                  <dd className="smallInputField">
-                    There should be avatars and an add button here!
-                    </dd>
-                </div> */}
-
-                <InputSmall
-                  title="Meeting name"
-                  inputType="text"
-                  cval={this.state.meetingName}
-                  smallInputValue={(e) => this.settingValueStates("meetingName", e)}
-                />
-                <DatePicker 
-                showTime={{
-                  hideDisabledOptions: true,
-                }}
-                
-                value={moment(this.state.meetingDate, dateFormat)}
-                format={dateFormat}
-                onChange={(date, dateString) => {
-                  this.settingValueStates("meetingDate", moment(date,dateFormat))
-                } } />
+                  className="datePicker"
+                  value={moment(this.state.meetingDate, dateFormat)}
+                  format={dateFormat}
+                  onChange={(date, dateString) => {
+                    this.settingValueStates("meetingDate", moment(date, dateFormat))
+                  }} />
 
 
                 <InputSmall
@@ -93,35 +82,42 @@ class UpdateMeeting extends React.Component {
                   cval={this.state.fundingRequest}
                   smallInputValue={(e) => this.settingValueStates("fundingRequest", e)}
                 />
-                <InputLarge
-                  title="Goal"
-                  cval={this.state.meetingGoal}
-                  largeInputValue={(e) => this.settingValueStates("meetingGoal", e)}
-                />
-       {/*        <InputLarge
-                  title="Feedback"
-                  cval={this.state.meetingFeedback}
-                  largeInputValue={(e) => this.settingValueStates("meetingFeedback", e)}
-       />*/}
 
+                {meetingData && !this.state.meetingName
+                  ? (
+                    <InputLarge
+                      title="Goal"
+                      cval={this.state.meetingGoal}
+                      largeInputValue={(e) => this.settingValueStates("meetingGoal", e)}
+                    />
+                  )
+                  : (
+                    <InputLarge
+                      title="Goal"
+                      cval={this.state.meetingName}
+                      largeInputValue={(e) => this.settingValueStates("meetingName", e)}
+                    />
+                  )}
+
+                <dt className="dt">Feedback</dt>
                 <Editor
-
-                initialValue={this.state.meetingFeedback}
-                init={{
-                  height: 500,
-                  menubar: false,
-                  plugins: [
-                    'advlist autolink lists link image charmap print preview anchor',
-                    'searchreplace visualblocks code fullscreen',
-                    'insertdatetime media table paste code help wordcount',
-                  ],
-                  toolbar:
-              'undo redo | formatselect | bold italic backcolor | \
+                  className="feedbackEditor"
+                  initialValue={this.state.meetingFeedback}
+                  init={{
+                    height: 500,
+                    menubar: false,
+                    plugins: [
+                      'advlist autolink lists link image charmap print preview anchor',
+                      'searchreplace visualblocks code fullscreen',
+                      'insertdatetime media table paste code help wordcount',
+                    ],
+                    toolbar:
+                      'undo redo | formatselect | bold italic backcolor | \
               alignleft aligncenter alignright alignjustify | \
               bullist numlist outdent indent | removeformat | help',
-                }}
-                onEditorChange={(content) => this.settingValueStates("meetingFeedback", content)}
-              />
+                  }}
+                  onEditorChange={(content) => this.settingValueStates("meetingFeedback", content)}
+                />
 
                 {/*
                 <div>
@@ -131,7 +127,7 @@ class UpdateMeeting extends React.Component {
                 */}
 
               </dl>
-              <div className="addMeetingButtons">
+              <div className="meetingButtons">
                 <button onClick={this.saveData} className="button button--4">Send meeting</button>
                 <button onClick={editMeetingCancel} className="button button--link">Cancel</button>
               </div>
