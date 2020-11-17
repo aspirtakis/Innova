b5168589b73d408abd247322b39253ad427aa47b
SB <47176495+SBDutchie@users.noreply.github.com>
2020-11-17 11:43:19 +0100

    styling PO Actions screen

--- app/containers/Onboarding/index.js
+++ app/containers/Onboarding/index.js
@@ -20,7 +20,7 @@ import saga from './saga';
 import messages from './messages';
 import KpnSmallInput from './components/kpnSmallInput';
 import KpnLargeInput from './components/kpnLargeInput';
-import { Popover,List, Avatar, Button, Skeleton,Tabs } from 'antd';
+import { Popover, List, Avatar, Button, Skeleton, Tabs } from 'antd';
 import './ideaOnboardingFormStyles.css';
 import Votes from './votes';
 
@@ -49,8 +49,8 @@ class Onboarding extends React.Component {
     this.state = {
 
       openAddform: false, // button always available
-      list:[],
-      selectedItem:null,
+      list: [],
+      selectedItem: null,
     };
   }
 
@@ -59,52 +59,52 @@ class Onboarding extends React.Component {
 
     //console.log(this.props.user);
     if (this.props.user && this.props.user.session_token.length > 0) {
-        this.getData();
+      this.getData();
     }
     if (!this.props.user && !this.props.user.session_token.length > 0) {
-        location.href = '/';
+      location.href = '/';
     }
 
-}
+  }
 
-closeandReload = () =>{
-  this.getData();
-  this.setState({ openAddform: false });
-  this.setState({selectedItem:null});
+  closeandReload = () => {
+    this.getData();
+    this.setState({ openAddform: false });
+    this.setState({ selectedItem: null });
 
-}
+  }
 
   getData = () => {
     this.props.dispatch(sessionCheck());
-       fetch(onboardingUrl, {
-           method: 'GET',
-           headers: {
-               Accept: 'application/json',
-               'X-DreamFactory-API-Key': apptoken,
-               'X-DreamFactory-Session-Token': this.props.user.session_token,
-               'Cache-Control': 'no-cache',
-               'Content-Type': 'application/json',
-           },
-       })
-           .then((response) => {
-               if (!response.ok) {
-                   throw Error(response.statusText);
-               }
-               return response;
-           })
-           .then(response => response.json())
-           .then((taskData) => {
-               const datas = taskData.resource;
-             //console.log(datas);
-             this.setState({list:datas})
-           })
-           .catch(taskData => console.log(taskData));
-   };
+    fetch(onboardingUrl, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'X-DreamFactory-API-Key': apptoken,
+        'X-DreamFactory-Session-Token': this.props.user.session_token,
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response;
+      })
+      .then(response => response.json())
+      .then((taskData) => {
+        const datas = taskData.resource;
+        //console.log(datas);
+        this.setState({ list: datas })
+      })
+      .catch(taskData => console.log(taskData));
+  };
 
 
-   addNewFunnelTask = values => {
-     const data = this.state.selectedItem;
-     const datastring = JSON.stringify(data);
+  addNewFunnelTask = values => {
+    const data = this.state.selectedItem;
+    const datastring = JSON.stringify(data);
     this.setState({ spinning: true });
     fetch(tasksUrl, {
       method: 'POST',
@@ -130,7 +130,7 @@ closeandReload = () =>{
             coach: "NotSet",
             sponsor: "1ppl",
             spnsr: "NotSet",
-           ideaData: datastring,
+            ideaData: datastring,
           },
         ],
       }),
@@ -153,32 +153,32 @@ closeandReload = () =>{
   archiveIdea = () => {
     const url4 = `${onboardingUrl}/${this.state.selectedItem.id}`;
     fetch(url4, {
-        method: 'PATCH',
-        headers: {
-            Accept: 'application/json',
-            'X-DreamFactory-API-Key': apptoken,
-            'X-DreamFactory-Session-Token': this.props.user.session_token,
-            'Cache-Control': 'no-cache',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          status:"ARCHIVE",
-        }),
+      method: 'PATCH',
+      headers: {
+        Accept: 'application/json',
+        'X-DreamFactory-API-Key': apptoken,
+        'X-DreamFactory-Session-Token': this.props.user.session_token,
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        status: "ARCHIVE",
+      }),
     })
-        .then((response) => {
-            if (!response.ok) {
-                throw Error(response.statusText);
-            }
-            return response;
-        })
-        .then(response => response.json())
-        .then((taskData) => {
-            this.getData();
-            this.setState({selectedItem:null})
-            this.setState({visible:false});
-        })
-        .catch(taskData => console.log(taskData));
-};
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response;
+      })
+      .then(response => response.json())
+      .then((taskData) => {
+        this.getData();
+        this.setState({ selectedItem: null })
+        this.setState({ visible: false });
+      })
+      .catch(taskData => console.log(taskData));
+  };
 
   render() {
     const { openAddform } = this.state;
@@ -193,109 +193,114 @@ closeandReload = () =>{
 
 
 
-      
-        {!openAddform && 
-          <div style={{padding:25}}>
-          <div className="row">
 
-          <button onClick={() => this.setState({openAddform:true})}>Add New Idea</button>
+        {!openAddform &&
+          <div style={{ padding: 25 }}>
+            <div className="row">
 
-        </div>
+              <button onClick={() => this.setState({ openAddform: true })}>Add New Idea</button>
 
-          <div className="row"> 
-          <div className="col col--6" >
-
-          <List
-          style={{maxWidth:500}}
-          itemLayout="vertical"
-          size="large"
-          dataSource={ideas}
-          footer={
-            <div>
-              <b>Backlog Ideas</b> 
             </div>
-          }
-          renderItem={item => (
-            <List.Item
-              key={item.Title}
-              extra={
-                <img
-                onClick={() => this.setState({selectedItem:item})}
-                  width={272}
-                  alt="logo"
-                  src="https://images.unsplash.com/photo-1586980368323-8ce5db4c85ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2815&q=80"
+
+            <div className="row">
+              <div className="col col--6" >
+
+                <List
+                  style={{ maxWidth: 500 }}
+                  itemLayout="vertical"
+                  size="large"
+                  dataSource={ideas}
+                  footer={
+                    <div>
+                      <b>Backlog Ideas</b>
+                    </div>
+                  }
+                  renderItem={item => (
+                    <List.Item
+                      key={item.Title}
+                      extra={
+                        <img
+                          onClick={() => this.setState({ selectedItem: item })}
+                          width={272}
+                          alt="logo"
+                          src="https://images.unsplash.com/photo-1586980368323-8ce5db4c85ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2815&q=80"
+                        />
+                      }
+                    >
+                      <List.Item.Meta
+                        avatar={<Avatar src={item.avatar} />}
+                        title={<a href={item.href}>{item.Title}</a>}
+                        description={item.OwnerFirstName}
+
+                      />
+
+                      <button   >Vote</button>
+                      <button>Innovate</button>
+                    </List.Item>
+                  )}
                 />
-              }
-            >
-              <List.Item.Meta
-                avatar={<Avatar src={item.avatar} />}
-                title={<a href={item.href}>{item.Title}</a>}
-                description={item.OwnerFirstName}
-    
-              />
 
-              <button   >Vote</button>
-              <button>Innovate</button>
-            </List.Item>
-          )}
-        />
-          
-         </div>
-         <div className="col col--6" >
+              </div>
+              <div className="col col--6" >
 
-{this.state.selectedItem && 
-         <Tabs tabBarStyle={{borderBlockColor:"#009900", color:'green'}} >
-         <TabPane  tab={<span className="titlesTab"> General</span>} key="1">
-         {this.state.selectedItem && <Votingform saveReload={this.closeandReload} item={this.state.selectedItem}></Votingform>} 
-         </TabPane>
+                {this.state.selectedItem &&
+                  <Tabs tabBarStyle={{ borderBlockColor: "#009900", color: 'green' }} >
+                    <TabPane tab={<span className="titlesTab"> General</span>} key="1">
+                      {this.state.selectedItem && <Votingform saveReload={this.closeandReload} item={this.state.selectedItem}></Votingform>}
+                    </TabPane>
 
-         <TabPane  tab={<span className="titlesTab" >Votes</span>} key="2">
-         <div>
-         <Votes  item={this.state.selectedItem}></Votes>
-       </div>
-  
-         
-         </TabPane>
-         <TabPane  tab={<span className="titlesTab" >PO Actions</span>} key="3">
-         <div>
-         COACH AND PO AREA  <br/>
-         Here You can transform IDEA to Innovation Card <br/>
-         <br/>
-         <div>{this.state.selectedItem.Title}</div>
-         <div>{this.state.selectedItem.OwnerFirstName}</div>
-         <div>{this.state.selectedItem.ElevatorPitch}</div>
-        <br/>
-         <Popover
-         content={<div>
-          <div>You are trasfering Idea to Funnel - Idea Will be archived and will not be visible anymore </div> <br/>
-          <div>Are you sure ?</div>
-          <button onClick={this.addNewFunnelTask}>Send</button>
-          <a onClick={() => this.setState({visible:false})}>Cancel</a>
+                    <TabPane tab={<span className="titlesTab" >Votes</span>} key="2">
+                      <div>
+                        <Votes item={this.state.selectedItem}></Votes>
+                      </div>
+
+
+                    </TabPane>
+                    <TabPane tab={<span className="titlesTab" >PO Actions</span>} key="3">
+                      <div>
+                        <br />
+         Here you can approve of an idea and send it to the funnel!<br />
+                        <br />
+                        <dl className="dl">
+                          <dt>Idea name</dt>
+                          <dd>{this.state.selectedItem.Title}</dd>
+                          <dt>Idea owner</dt>
+                          <dd>{this.state.selectedItem.OwnerFirstName + " " + this.state.selectedItem.OwnerLastName}</dd>
+                          <dt>Elevator pitch</dt>
+                          <dd>{this.state.selectedItem.ElevatorPitch}</dd>
+                        </dl>
+                        <br />
+                        <Popover
+                          content={<div>
+                            <div>You are trasfering Idea to Funnel - Idea Will be archived and will not be visible anymore </div> <br />
+                            <div>Are you sure ?</div>
+                            <button onClick={this.addNewFunnelTask}>Send</button>
+                            <a onClick={() => this.setState({ visible: false })}>Cancel</a>
+                          </div>
+                          }
+                          title={"Idea " + this.state.selectedItem.Title}
+                          trigger="click"
+                          visible={this.state.visible}
+                          onVisibleChange={this.handleVisibleChange}
+                        >
+                          <Button onClick={() => this.setState({ visible: true })}>Approve and send to funnel Backlog</Button>
+                        </Popover>
+
+                      </div>
+
+
+                    </TabPane>
+                  </Tabs>
+                }
+
+
+              </div>
+            </div>
+
+
           </div>
-         }
-         title={"Idea "+this.state.selectedItem.Title }
-         trigger="click"
-         visible={this.state.visible}
-         onVisibleChange={this.handleVisibleChange}
-       >
-       <Button onClick={() => this.setState({visible:true})}>Approve and send to funnel Backlog</Button>
-       </Popover>
-   
-       </div>
-  
-         
-         </TabPane>
-         </Tabs>
+
         }
-
-         
-          </div>
-         </div>
-
-
-</div>
-          
-   }
         {openAddform && <Onboardingform closeForm={this.closeandReload} />}
 
       </div>
