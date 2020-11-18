0aaf4fc977c4f602dbda439d4cf97b5e7f067069
Agamemnon <aspirtakis@gmail.com>
2020-11-18 15:47:39 +0100

    rank on details

--- app/containers/Onboarding/index.js
+++ app/containers/Onboarding/index.js
@@ -218,7 +218,7 @@ return avatChars;
               <div className="col col--6" >
 
               <div className="table-toolbar">
-              <div className="table-toolbar__data">3 items</div>
+              <div className="table-toolbar__data">({ideas.length}) Ideas</div>
               <div className="table-toolbar__action">
                 <div className="input-field">
                   <div className="input-field__input">
@@ -286,10 +286,68 @@ return avatChars;
               </div>
               <div className="col col--6" >
 
+  
                 {this.state.selectedItem &&
+
+                  <div>
+
+                  <div className="table-toolbar">
+        
+                  <div className="table-toolbar__action">
+
+                  <Popover
+                  content={<div>
+                    <div>You are trasfering Idea to Funnel - Idea Will be archived and will not be visible anymore </div> <br />
+                    <div>Are you sure ?</div>
+                    <button onClick={this.addNewFunnelTask}>Send</button>
+                    <a onClick={() => this.setState({ visible: false })}>Cancel</a>
+                  </div>
+                  }
+                  title={"Idea " + this.state.selectedItem.Title}
+                  trigger="click"
+                  visible={this.state.visible}
+                  onVisibleChange={this.handleVisibleChange}
+                >
+                  <button style={{maxWidth:100}} className="button button--secondary" onClick={() => this.setState({ visible: true })}>Approve</button>
+                </Popover>
+
+
+                <Popover
+                content={<div>
+                  <div>Idea Will be archived and will not be visible anymore </div> <br />
+                  <div>Are you sure ?</div>
+                  <button onClick={this.archiveIdea}>Archive</button>
+                  <a onClick={() => this.setState({ visibleArch: false })}>Cancel</a>
+                </div>
+                }
+                title={"Idea " + this.state.selectedItem.Title}
+                trigger="click"
+                visible={this.state.visibleArch}
+                onVisibleChange={this.handleVisibleChange}
+              >
+                <button style={{maxWidth:100}} className="button button--secondary" onClick={() => this.setState({ visibleArch: true })}>Reject</button>
+              </Popover>
+
+
+                  </div>
+                </div>
+    
                   <Tabs tabBarStyle={{ borderBlockColor: "#009900", color: 'green' }} >
                     <TabPane tab={<span className="titlesTab"> General</span>} key="1">
+
+
+
+
+
+
+
+
+                    
+                    
                       {this.state.selectedItem && <Votingform saveReload={this.closeandReload} item={this.state.selectedItem}></Votingform>}
+
+
+               
                     </TabPane>
 
                     <TabPane tab={<span className="titlesTab" >Votes</span>} key="2">
@@ -329,6 +387,7 @@ return avatChars;
                       </div>
                     </TabPane>
                   </Tabs>
+                  </div>
                 }
               </div>
             </div>
